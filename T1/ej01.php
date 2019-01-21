deb211a84f086c08ddbe24b6cba684b8d53b2519
fgironpe <38102060+fgironpe@users.noreply.github.com>
2019-01-21 20:21:21 +0100

    Creado repositorio y workspace Visual Studio Code

---
+++ T1/ej01.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Pedir infinitos números enteros (hasta que se introduzca) el cero. Indicar finalmente cuál fue el máximo y cuál fue el mínimo.
+	Ejemplo:
+	Introduce n: -9
+	Introduce n: 1
+	Introduce n: 7
+	Introduce n: -10
+	Introduce n: 3
+	Introduce n: 0
+	Máximo: 7
+	Mínimo: -10
+*/
+
