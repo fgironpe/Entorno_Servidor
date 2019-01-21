e50832c6ed1069033cdd743772454122a9a9cf61
fgironpe <38102060+fgironpe@users.noreply.github.com>
2019-01-21 22:08:12 +0100

    Subido ej02.php

---
+++ T1/ej02.php
@@ -0,0 +1,19 @@
+<?php
+/*
+Dado un número “n”, dibujar líneas desde n caracteres hasta un carácter disminuyendo en un carácter cada línea que se dibuje. Repetir el patrón “+” “-” “.” en cada carácter que se imprima.
+	Ejemplo:
+Introduce n: 7
+	+-.+-.+
+	-.+-.+
+	-.+-.
+	+-.+
+	-.+
+	-.
+	+
+	
+Ejemplo 
+Introduce n: 2
+	+-
+	.
+
+*/
